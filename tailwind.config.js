40a5e8f363ba524f6a7b6f05e94420d157690540
Cairo <cairofelipedev@gmail.com>
2023-12-28 21:07:27 -0300

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
+
+module.exports = {
+  content: ['./src/**/*.js'],
+  darkMode: 'media',
+  theme: {
+    extend: {
+      colors: {
+        'rd': '#F8A12C',
+      },
+      fontFamily: {
+        sans: ['Nunito', ...defaultTheme.fontFamily.sans],
+      },
+    },
+  },
+  variants: {
+    extend: {
+      opacity: ['disabled'],
+    },
+  },
+  plugins: [require('@tailwindcss/forms')],
+}
